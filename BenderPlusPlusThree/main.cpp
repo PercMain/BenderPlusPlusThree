023a8dcbb5821bf8f8bd07c3f16054dfcb538f78
Ben Lewson <benlewson@Bens-MacBook-Pro-5.local>
2016-04-26 20:45:41 -0500

    Initial Commit

---
+++ BenderPlusPlusThree/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BenderPlusPlusThree
+//
+//  Created by Ben Lewson on 4/26/16.
+//  Copyright (c) 2016 Ben Lewson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
