ef0b669130a6c0b036c57dc0e0dc5fc6ac3429d0
Ben Lewson <benlewson@Bens-MacBook-Pro-5.local>
2016-05-02 10:24:27 -0500

    Initial Commit of Bender Class

---
+++ BenderPlusPlusThree/Bender.cpp
@@ -0,0 +1,9 @@
+//
+//  Bender.cpp
+//  BenderPlusPlusThree
+//
+//  Created by Ben Lewson on 5/2/16.
+//  Copyright (c) 2016 Ben Lewson. All rights reserved.
+//
+
+#include "Bender.h"
---
+++ BenderPlusPlusThree/Bender.h
@@ -0,0 +1,24 @@
+//
+//  Bender.h
+//  BenderPlusPlusThree
+//
+//  Created by Ben Lewson on 5/2/16.
+//  Copyright (c) 2016 Ben Lewson. All rights reserved.
+//
+
+#ifndef Bender_h
+#define Bender_h
+
+#include <iostream>
+#include "SvgPacker.h"
+
+class Bender
+{
+public:
+    //Constructor
+    Bender();
+    
+    string generateBend(float height, float width, int numberOfCuts);
+};
+
+#endif /* defined(__BenderPlusPlusThree__Bender__) */
